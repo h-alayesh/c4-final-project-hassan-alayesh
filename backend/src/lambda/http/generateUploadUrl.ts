fd58b1c01197375ebaab066297d0a2aedece3342
Hassan Ayesh <h_alayesh@yahoo.com>
2019-11-03 18:51:15 +0300

    Update Serverless Yaml & Functions
    Update Serverless Yaml & Functions

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -2,9 +2,62 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
+import * as AWS from 'aws-sdk'
+
+import * as AWSXRay from 'aws-xray-sdk'
+
+import { parseUserId } from '../../auth/utils'
+
+import * as uuid from 'uuid'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+const docClient = new XAWS.DynamoDB.DocumentClient()
+const s3 = new XAWS.S3({
+  signatureVersion: 'v4'
+})
+
+const todosTable = process.env.TODOS_TABLE
+const bucketName = process.env.TODOS_IMAGES_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+  const userId = parseUserId(jwtToken)
+  const imageId = uuid.v4()
+
+  await docClient.update({
+    TableName: todosTable,
+    Key: {
+      "userId": userId,
+      "todoId": todoId
+    },
+    ExpressionAttributeValues: {
+      "attachmentUrl": `https://${bucketName}.s3.amazonaws.com/${imageId}`
+    },
+  }).promise()
+
+  const url = s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: imageId,
+    Expires: urlExpiration
+  })
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      signedUrl: url
+    })
+  }
+
+  //return undefined
 }

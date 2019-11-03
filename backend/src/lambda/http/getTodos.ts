2865736927ab9af990156ab2130315047475e133
h-alayesh <55011454+h-alayesh@users.noreply.github.com>
2019-11-03 10:52:18 +0300

    Update Lambda Fuctions
    Update Lambda Fuctions

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -2,6 +2,53 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
+import * as AWS  from 'aws-sdk'
+
+import * as AWSXRay from 'aws-xray-sdk'
+
+import { parseUserId } from '../../auth/utils'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+const docClient = new XAWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+  const userId = parseUserId(jwtToken)
+
+  if (userId = null) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: 'User does not exist'
+      })
+    }
+  }
+
+  const result = await docClient.query({
+    TableName: todosTable,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    },
+    ScanIndexForward: false
+  }).promise()
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items: result.Items
+    })
+  }
+
 }
--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -4,10 +4,51 @@ import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } f
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 
+import * as AWS  from 'aws-sdk'
+
+import * as AWSXRay from 'aws-xray-sdk'
+
+import { parseUserId } from '../../auth/utils'
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+const docClient = new XAWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+
+  const authorization = event.headers.Authorization
+    const split = authorization.split(' ')
+    const jwtToken = split[1]
+    const userId = parseUserId(jwtToken)
+
+    await docClient.update({
+      TableName: todosTable,
+      Key:{
+        "userId": userId,
+        "todoId": todoId
+    },
+    ExpressionAttributeValues:{
+      "name": updatedTodo.name,
+      "dueDate": updatedTodo.dueDate,
+      "done": updatedTodo.done
+  },
+    }).promise()
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: 'Todo Updated'
+    }
+
+
+  //return undefined
 }

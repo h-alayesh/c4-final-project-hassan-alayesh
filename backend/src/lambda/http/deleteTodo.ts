674cefa744edd133e0eb22e52b0acbfee3f41fc5
h-alayesh <55011454+h-alayesh@users.noreply.github.com>
2019-11-13 11:53:31 +0300

    Create todoAccess class
    Create todoAccess class

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -2,28 +2,16 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
-import * as AWS  from 'aws-sdk'
+import { TodoAccess } from '../../dataLayer/todoAccess'
 
-import * as AWSXRay from 'aws-xray-sdk'
-
-const XAWS = AWSXRay.captureAWS(AWS)
-
-const docClient = new XAWS.DynamoDB.DocumentClient()
-const todosTable = process.env.TODOS_TABLE
+const todoAccess = new TodoAccess()
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
 
     // TODO: Remove a TODO item by id
 
-    const delTodoItem = {
-
-      "todoId": todoId
-    }
-    await docClient.delete({
-      TableName: todosTable,
-      Key: delTodoItem 
-    }).promise()
+    await todoAccess.deleteTodo( todoId )
 
     return {
       statusCode: 200,
